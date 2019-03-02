ccddfad25900daced0524e05e66ad2a1eed16507
Glitch (cyber-runner) <none>
2019-03-02 16:50:57 +0000

    🛎🍶 Checkpoint ./komutlar/filtre-sil.js:1749922/238

--- komutlar/filtre-sil.js
+++ komutlar/filtre-sil.js
@@ -8,15 +8,27 @@ const s = await db.fetch(`filtre_${message.guild.id}`)
 
 if(s === null) return message.channel.send('Eklenmiş filtre bulunmuyor.')
   
+  
+  /*
  let kelimeler = args[0]
   
   var a = db.delete(`filtre_${message.guild.id}`)
   
- 
+ */
+  
+  
+  let x = args[0] //silinecek şey yani
+let arr = []
+db.fetch(`filtre_${message.guild.id}`).forEach(v => {
+if (v !== x) {
+arr.push(v)
+}
+})
+db.set(`filtre_${message.guild.id}`, arr)
 
     var embed = new Discord.RichEmbed()
     .setColor("RANDOM")
-   .setDescription(`Sunucudaki **tüm** filtreler silindi`) 
+   .setDescription(`Sunucudaki ${args[0]} filtresi silindi`) 
     message.channel.send(embed)
   
 };
