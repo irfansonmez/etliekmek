0af51f9610ce90f4a18e5e0b9d911079c437154c
Glitch (cyber-runner) <none>
2019-03-03 12:08:05 +0000

    👽💋 Checkpoint ./komutlar/davet.js:1749922/5

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -9,7 +9,7 @@ exports.run = async (client, message) => {
   const davet = new Discord.RichEmbed()
 .setColor("RANDOM")
 .setAuthor(`${client.user.username} - LİNKLERİ`, client.user.avatarURL)
-.setDescription(`[Bot davet linki](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2146958847) \n[Destek sunucusu](https://discord.gg/vnWgkX3)  \n[DBL sayfası](https://discordbots.org/bot/${client.user.id}) \n[DBL Oy sayfası](https://discordbots.org/bot/${client.user.id})
+.setDescription(`[Bot davet linki](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2146958847) \n[Destek sunucusu](https://discord.gg/vnWgkX3)  \n[DBL sayfası](https://discordbots.org/bot/${client.user.id}) \n[DBL Oy sayfası](https://discordbots.org/bot/${client.user.id}/vote)
 `)
 message.channel.send(davet)
 };
