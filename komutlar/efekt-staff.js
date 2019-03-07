9ed06f2e185b48fb7d6e83f68649ca4505f6a2fa
Glitch (seed-satin) <none>
2019-03-07 05:13:20 +0000

    🐁⭐️ Checkpoint ./komutlar/efekt-partner.js:1749922/1212 ./komutlar/efekt-bugHunter.js:1749922/876 ./komutlar/efekt-event.js:1749922/404 ./komutlar/efekt-nitro.js:1749922/400 ./komutlar/efekt-staff.js:1749922/400 ./komutlar/efekt-trinity.js:1749922/188

--- komutlar/efekt-staff.js
+++ komutlar/efekt-staff.js
@@ -38,7 +38,7 @@ if(check == 1) {
             image.resize(400, 400)  
             //image.greyscale()
          //   image.gaussian(1)
-            Jimp.read(`https://github.com/batuhan121/botumuzg-zelbroo-/blob/master/efektler/discord/staff${mesaj}.png`, (err, avatar) => {
+            Jimp.read(`https://raw.githubusercontent.com/batuhan121/botumuzg-zelbroo-/master/efektler/discord/staff${mesaj}.png`, (err, avatar) => {
                 avatar.resize(400, 400)
                 image.composite(avatar, 0, 0).write(`./img/efektler/${user.id}.png`);
                 setTimeout(function() {
@@ -57,7 +57,7 @@ if(check == 1) {
             image.resize(400, 400)  
             //image.greyscale()
           //  image.gaussian(1)
-            Jimp.read(`https://github.com/batuhan121/botumuzg-zelbroo-/blob/master/efektler/discord/staff${mesaj}.png`, (err, avatar) => {
+            Jimp.read(`https://raw.githubusercontent.com/batuhan121/botumuzg-zelbroo-/master/efektler/discord/staff${mesaj}.png`, (err, avatar) => {
                 avatar.resize(400, 400)
                 image.composite(avatar, 0, 0).write(`./img/efektler/${user.id}.png`);
                 setTimeout(function() {
