1ba0034f28c506833f2210a16a37fc6e52b3ffe8
Glitch (seed-satin) <none>
2019-03-10 08:57:52 +0000

    🎩☔️ Checkpoint ./komutlar/ayarlar.js:1749922/1771

--- komutlar/ayarlar.js
+++ komutlar/ayarlar.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
   const a = args[0]
   
   //if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`);
-  
+  let y = await client.emojis.get(client.emojiler.yan);
   let prefix2 = await db.fetch(`prefix_${message.guild.id}`) || client.ayarlar.prefix;
   let prefix = await db.fetch(`prefix_${message.guild.id}`) || client.ayarlar.prefix;
   var ac = client.emojis.get(client.emojiler.evet)
@@ -23,28 +23,32 @@ if (!a && a !== "destek" && a !== "kapat" && a !== "liste" && a !== "support" &&
   
   const sayfa = [`
 
+   **Destek sistemi **${prefix}ayarlar destek** Yazarak destek ayarlarını görebilirsiniz**
 
- Destek sistemi **${prefix}ayarlar destek** Yazarak destek ayarlarını görebilirsiniz
-  **unucu prefixi** ${prefix2}
-  **Giriş çıkış kanalı** ${db.has(`gc_${message.guild.id}`) ? ac + message.guild.channels.get(db.fetch(`gc_${message.guild.id}`)) : `${ka} Ayarlanmamış **${prefix}giriş-çıkış-ayarla**} 
-  Mod log kanalı ${db.has(`mLog_${message.guild.id}`) ? `${ac} ${db.fetch(`mLog_${message.guild.id}`)}` : `${ka} Ayarlanmamış **${prefix}mod-log-ayarla** `}
-  Log kanalı ${db.has(`log_${message.guild.id}`) ? ac + message.guild.channels.get(db.fetch(`log_${message.guild.id}`)) : `${ka} Ayarlanmamış **${prefix}log-ayarla**`}
-  Link engeli ${db.has(`linkE_${message.guild.id}`) ? `${ac} Açık` : `${ka} Ayarlanmamış **${prefix}link-engelle**`}
-  Küfür engeli ${db.has(`küfürE_${message.guild.id}`) ? `${ac} Açık` : `${ka} Ayarlanmamış **${prefix}küfür-engelle**`}
-  Büyük harf engeli ${db.has(`capsE_${message.guild.id}`) ? `${ac} Açık` : `${ka} Ayarlanmamış **${prefix}büyükharf-engelle**` }
-  Otorol ${db.has(`otoR_${message.guild.id}`) ? `${ac} \`@${message.guild.roles.get(db.fetch(`otoR_${message.guild.id}`)).name}\`` : `${ka} Ayarlanmamış **${prefix}oto-rol**`}
-  Otorol Kayıt Kanalı ${db.has(`otoRK_${message.guild.id}`) ? `${ac} \`@${message.guild.channels.get(db.fetch(`otoRK_${message.guild.id}`)).name}\`` : `${ka} Ayarlanmamış **${prefix}oto-rol-kanal**`}
-  Susturma rolü ${db.has(`sRol_${message.guild.id}`) ? `${ac} \`@${message.guild.roles.get(db.fetch(`sRol_${message.guild.id}`)).name}\`` : `${ka} Ayarlanmamış **${prefix}sustur-rol-ayarla**`}
-  Sayaç kanalı ${db.has(`sKanal_${message.guild.id}`) ? `${ac} ${db.fetch(`sKanal_${message.guild.id}`)}` : `${ka} Ayarlanmamış **${prefix}sayaç-kanal-ayarla** `}
-  Sayaç ${db.has(`sayac_${message.guild.id}`) ? `${ac} ${db.fetch(`sayac_${message.guild.id}`)}` : `${ka} Ayarlanmamış **${prefix}sayaç-ayarla**`}
-  Davet kanalı ${db.has(`dKanal_${message.guild.id}`) ? ac + message.guild.channels.get(db.fetch(`dKanal_${message.guild.id}`)) : `${ka} Ayarlanmamış **${prefix}davet-kanal-ayarla**`}
- 
-  Otomatig tag ${db.has(`tagB_${message.guild.id}`) ? db.fetch(`tagB_${message.guild.id}`) : `${ka} Ayarlanmamış **${prefix}tag-ayarla**`}
-
-  Giriş mesajı ${db.has(`girisM_${message.guild.id}`) ? db.fetch(`girisM_${message.guild.id}`).replace("{kullanıcı}", "**{kullanıcı}**").replace("{user}", "**{user}**") : `${ka} Ayarlanmamış **${prefix}giriş-mesaj-ayarla**`}
-  Çıkış mesajı ${db.has(`cikisM_${message.guild.id}`) ? db.fetch(`cikisM_${message.guild.id}`).replace("{kullanıcı}", "**{kullanıcı}**").replace("{user}", "**{user}**") : `${ka} Ayarlanmamış **${prefix}çıkış-mesaj-ayarla**`}
-
+${y}**Sunucu prefixi** ${prefix2}
+${y}**Mod log kanalı** ${db.has(`mLog_${message.guild.id}`) ? `${ac} ${db.fetch(`mLog_${message.guild.id}`)}` : `${ka} Ayarlanmamış **${prefix}mod-log-ayarla** `}
+${y}**Log kanalı** ${db.has(`log_${message.guild.id}`) ? ac + message.guild.channels.get(db.fetch(`log_${message.guild.id}`)) : `${ka} Ayarlanmamış **${prefix}log-ayarla**`}
+${y}**Davet kanalı** ${db.has(`dKanal_${message.guild.id}`) ? ac + message.guild.channels.get(db.fetch(`dKanal_${message.guild.id}`)) : `${ka} Ayarlanmamış **${prefix}davet-kanal-ayarla**`}  
+${y}**Link engeli** ${db.has(`linkE_${message.guild.id}`) ? `${ac} Açık` : `${ka} Ayarlanmamış **${prefix}link-engelle**`}
+${y}**Küfür engeli** ${db.has(`küfürE_${message.guild.id}`) ? `${ac} Açık` : `${ka} Ayarlanmamış **${prefix}küfür-engelle**`}
+${y}**Büyük harf engeli** ${db.has(`capsE_${message.guild.id}`) ? `${ac} Açık` : `${ka} Ayarlanmamış **${prefix}büyükharf-engelle**` }
+${y}**Otorol** ${db.has(`otoR_${message.guild.id}`) ? `${ac} \`@${message.guild.roles.get(db.fetch(`otoR_${message.guild.id}`)).name}\`` : `${ka} Ayarlanmamış **${prefix}oto-rol**`}
+${y}**Otorol Kayıt Kanalı** ${db.has(`otoRK_${message.guild.id}`) ? `${ac} \`@${message.guild.channels.get(db.fetch(`otoRK_${message.guild.id}`)).name}\`` : `${ka} Ayarlanmamış **${prefix}oto-rol-kanal**`}
+${y}**Susturma rolü** ${db.has(`sRol_${message.guild.id}`) ? `${ac} \`@${message.guild.roles.get(db.fetch(`sRol_${message.guild.id}`)).name}\`` : `${ka} Ayarlanmamış **${prefix}sustur-rol-ayarla**`}
+${y}**Sayaç kanalı** ${db.has(`sKanal_${message.guild.id}`) ? `${ac} ${db.fetch(`sKanal_${message.guild.id}`)}` : `${ka} Ayarlanmamış **${prefix}sayaç-kanal-ayarla** `}
+${y}**Sayaç** ${db.has(`sayac_${message.guild.id}`) ? `${ac} ${db.fetch(`sayac_${message.guild.id}`)}` : `${ka} Ayarlanmamış **${prefix}sayaç-ayarla**`}
+${y}**Otomatig tag ${db.has(`tagB_${message.guild.id}`) ? db.fetch(`tagB_${message.guild.id}`) : `${ka} Ayarlanmamış **${prefix}tag-ayarla**`}
+${y}**Giriş Çıkış kanalı** ${db.has(`gc_${message.guild.id}`) ? `${ac} ${client.channels.get(db.fetch(`gc_${message.guild.id}`))}` : `${ka} Ayarlanmamış **${prefix}giriş-çıkış-ayarla** `}
+${y}**Giriş mesajı ${db.has(`girisM_${message.guild.id}`) ? db.fetch(`girisM_${message.guild.id}`).replace("{kullanıcı}", "**{kullanıcı}**").replace("{user}", "**{user}**") : `${ka} Ayarlanmamış **${prefix}giriş-mesaj-ayarla**`}
+${y}**Çıkış mesajı ${db.has(`cikisM_${message.guild.id}`) ? db.fetch(`cikisM_${message.guild.id}`).replace("{kullanıcı}", "**{kullanıcı}**").replace("{user}", "**{user}**") : `${ka} Ayarlanmamış **${prefix}çıkış-mesaj-ayarla**`}
 `] 
+  
+  const ayarReis = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setDescription(sayfa)
+  .setTimestamp()
+  message.channel.send(ayarReis)
+  
   var embed = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setAuthor(`» ${message.guild.name} Sunucu ayarları «`, `https://cdn.discordapp.com/emojis/456224342427041803.png?v=1`)
@@ -67,7 +71,7 @@ if (!a && a !== "destek" && a !== "kapat" && a !== "liste" && a !== "support" &&
 
   .addField('Giriş mesajı', db.has(`girisM_${message.guild.id}`) ? db.fetch(`girisM_${message.guild.id}`).replace("{kullanıcı}", "**{kullanıcı}**").replace("{user}", "**{user}**") : `${ka} Ayarlanmamış **${prefix}giriş-mesaj-ayarla**`)
   .addField('Çıkış mesajı', db.has(`cikisM_${message.guild.id}`) ? db.fetch(`cikisM_${message.guild.id}`).replace("{kullanıcı}", "**{kullanıcı}**").replace("{user}", "**{user}**") : `${ka} Ayarlanmamış **${prefix}çıkış-mesaj-ayarla**`)
-  message.channel.send(embed)
+  //message.channel.send(embed)
   return;
 }
   
