65a9c92ed78c9c4ef1f34bad3b5b108c01a2ab0c
Glitch (seed-satin) <none>
2019-03-12 11:41:40 +0000

    Rewound to commit f5c37ccfab029cb51af455620f4a1b33ad1e2e30 + 0 edits

--- index.js
+++ index.js
@@ -107,6 +107,20 @@ client.ayarlar = {
         "versiyon": "0.0.1",
         "prefix": "?",
         "veritabanı": "clientt.veritabanı",
+        "yenilik": [`**FİLTRE SİSTEMİ**
+{prefix}filtre-ekle
+{prefix}filtre-sil
+{prefix}filtre-liste
+**FİLTRE BİLGİ**
+Sunucunuzda istediğiniz bir kelimeyi yasaklamanıza yarar.
+
+**ÖZEL KOMUT SİSTEMİ**
+**{prefix}komut-ekle
+**{prefix}komut-sil**
+**{prefix}komut-liste**
+
+**BİLGİ**
+Sunucunuza özel komut eklemeye yarar.`],
 "renk":  "DARKBLUE",
 "version":  "2.0.0",
  
