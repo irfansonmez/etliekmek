bbd90a9c1584ad753ee813a6297bd9e987974e95
Glitch (seed-satin) <none>
2019-03-17 00:03:29 +0000

    🖇☠️ Checkpoint ./komutlar/komut-yasakla.js:1749922/1034

--- komutlar/komut-yasakla.js
+++ komutlar/komut-yasakla.js
@@ -6,22 +6,25 @@ exports.run = async (client, message, args) => {
 let prefix;
 if (p == null) prefix = client.ayarlar.prefix
   else prefix = p
-  if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(client.emojis.get(client.emojiler.hayır) + ' Bu komut için yeterli iznin **yok!**')
+  if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`${client.emojis.get(client.emojiler.hayır)}  **bu komut için yeterli iznin yok!**`)
   
   let command = args[0]
-  if (!command) return message.channel.send(client.emojis.get(client.emojiler.hayır) + ' Lütfen bir **komut adı** girin! Şarkı komutlarını iptal etmek için; `+komut-yasakla şarkı`')
+  if (!command) return message.channel.send(`${client.emojis.get(client.emojiler.hayır)} Lütfen bir **komut adı** girin! Şarkı komutlarını iptal etmek için; **komut-yasakla şarkı**`)
   
-  if (!client.commands.has(command)) return message.channel.send(client.emojis.get(client.emojiler.hayır) + ' Böyle bir **komut** bulunamadı!')
+  if (!client.commands.has(command)) return message.channel.send(`${client.emojis.get(client.emojiler.hayır)} Böyle bir **komut** bulunamadı!`)
+      if(command == 'yardım') return message.channel.send(`${client.emojis.get(client.emojiler.hayır)} Yardım komudunu yasaklayamazsın.`)
   if (client.commands.has(command)){
     command = client.commands.get(command);
     
+
+    
     if (db.has(`yasak_${message.guild.id}_${command.help.name}`)){
       await db.delete(`yasak_${message.guild.id}_${command.help.name}`)
-      message.channel.send(process.env.basarili + ' Komut **artık kullanılabilir!**')
+      message.channel.send(`${client.emojis.get(client.emojiler.evet)} Komut **artık kullanılabilir!**`)
     }else{
     
     await db.push(`yasak_${message.guild.id}_${command.help.name}`, command.help.name)
-    message.channel.send(`${process.env.basarili} Komut başarıyla **sunucuda yasaklandı!** Tekrar aktif etmek için; \`${prefix}komut-yasakla ${args[0]}\``)
+    message.channel.send(`${client.emojis.get(client.emojiler.evet)} Komut başarıyla **sunucuda yasaklandı!** Tekrar aktif etmek için; \`${prefix}komut-yasakla ${args[0]}\``)
     await db.push(`yasak_${message.guild.id}_${command.conf.aliases[0] ? command.conf.aliases.join(', ') : 'Bulunmuyor'}`, command.conf.aliases[0])
     }
   }
