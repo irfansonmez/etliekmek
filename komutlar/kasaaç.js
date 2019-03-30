ee6b0830990a411b7f7559b1ea15732994b2891b
Glitch (ryker-bot) <none>
2019-03-30 09:24:24 +0000

    🐉🚏 Checkpoint ./komutlar/kasaaç.js:1749922/3169

--- komutlar/kasaaç.js
+++ komutlar/kasaaç.js
@@ -1,9 +1,49 @@
 const Discord = require('discord.js'),
       db = require('quick.db');
 
+const değerli = [],
+değersiz = [],
+orta = [],
+çokDeğerli = [],
+çokDeğersiz = [];
+
+
+
 exports.run = async (client, message, args) => {
 
-
+  var şans = Math.floor(Math.random() * 101);
+  
+  
+  if(şans <= 5) {
+  message.channel.send(`çok değerli ${şans}`)
+  return
+  }
+  
+    if(şans <= 15) {
+  message.channel.send(`değerli ${şans}`)
+  return
+  
+  }
+  
+    if(şans <= 35) {
+  // ORTA
+    message.channel.send(`orta ${şans}`)
+  return
+  }
+  
+  
+   if(şans <= 50) {
+  // DEĞERSİZ
+    message.channel.send(`değersiz ${şans}`)
+  return
+  }
+  
+   if(şans <= 100) {
+  // ÇOK DEĞERSİZ
+    message.channel.send(`çok değersiz ${şans}`)
+  return
+  }
+  //  var sonuc = sans[Math.floor((Math.random() * sans.length))];
 
 }
 
