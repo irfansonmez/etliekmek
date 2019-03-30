13ef7269f473fd482327579076649b266a2625dc
Glitch (ryker-bot) <none>
2019-03-30 08:29:24 +0000

    🐾💨 Checkpoint ./komutlar/kasaaç.js:1749922/486

--- komutlar/kasaaç.js
+++ komutlar/kasaaç.js
@@ -6,3 +6,17 @@ exports.run = async (client, message, args) => {
 
 
 }
+
+exports.conf = {
+enabled: false,
+guildOnly: false,
+kategori: 'oyun',
+aliases: [],
+permLevel: 0,
+}
+
+exports.help = {
+    name: 'kasaaç',
+    description: 'Bir adet csgo kasası açarsınız',
+    usage: 'kasaaç',
+}
