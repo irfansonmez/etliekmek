e26016e86d29475d02b6ad7b4eeb93016735e1c4
Glitch (ryker-bot) <none>
2019-03-30 17:58:26 +0000

    🎤🐝 Checkpoint ./komutlar/yardım.js:1749922/11:2774423/1062 ./komutlar/yardım2.js:1749922/16324 ./watch.json:2774423/8

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,7 +4,7 @@ const db = require('quick.db');
 exports.run = async (bot, msg, args, dil) => {
   const client = bot
 
-  let x = args[0]
+  let arg = args[0]
 
  // let yana = await bot.emojis.get(bot.emojiler.yan);
 let yana = "-";
@@ -18,9 +18,24 @@ if (!arr.includes(x.conf.kategori)) {
 arr.push(x.conf.kategori)
 }
 })
-arr.map(k => k).join("\n")
+
+  let catsArr = arr.map(k => `[${i++}]: ${k}`)
   
-  msg.channel.sendCode
+  let i = 1
+  let cats = arr.map(k => `[${i++}]: ${k}`).join("\n")
+  
+  msg.channel.send(`# ${client.user.username} - Kategoriler
+
+${cats}
+
+> ${prefix}yardım [kategori/kategori numarası] yazarak komutları görebilirsiniz.
+`, {split: true, code: "md"})
+  
+  if (!client.commands.filter(x => x.conf.kategori === arg)[0]) return msg.channel.send(`**${arg}** ${isNaN(arg) ? "adlı" : "numaralı"} bir kategori bulunamadı!`)
+  
+  let list = isNaN(arg) ? client.commands.filter(x => x.conf.kategori === arg) : client.commands.filter(x => x.conf.kategori === catsArr[arg])
+  
+  msg.channel.send(list.map(k => `${k.help.name} :: ${}`), {split: true, code: "asciidoc"})
   
 }
 
@@ -35,5 +50,5 @@ exports.conf = {
 exports.help = {
   name: 'yardım',
   description: 'Botun komutlarını listeler',
-  usage: 'yardım'
+  usage: 'yardım [kategori]'
 };
