8776b97cda517cab05dd41ed40a3a8ef2b3d60d0
konya421 <49554823+konya421@users.noreply.github.com>
2019-09-25 20:49:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": "507803933557915652", //kendi IDınızı yazınız
     "admins": ["336869318874890241"],
     "support": ["336869318874890241"],
-    "token": "NTgwODMxODg4MDAwOTQyMTQz.XV0szA.ai5rDdGRWRFYsf1QKA0DBX0hFuA", //botunuzun tokenini yazınız
+    "token": "NTgwODMxODg4MDAwOTQyMTQz.XYuong.7jrKl-NV-jCwf9DfJGia33DegMc", //botunuzun tokenini yazınız
     "dashboard" : {
       "oauthSecret": "a4zpplghBRL3YGTGLUZLPWuRi6LZKMmb", //botunuzun secretini yazınız
       "callbackURL": `https://konyatra.herokuapp.com/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
