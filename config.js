e53239307d96715a98f747e54395855d8caee55f
konya421 <49554823+konya421@users.noreply.github.com>
2019-09-25 19:14:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const config = {
     "ownerID": "507803933557915652", //kendi IDınızı yazınız
     "admins": ["336869318874890241"],
     "support": ["336869318874890241"],
-    "token": "NTY0MDkzNDIzNzU0MjgwOTYw.XKi2-A.8NonFn5TRtv_I0IXsS5IBGOeqsM", //botunuzun tokenini yazınız
+    "token": "NTgwODMxODg4MDAwOTQyMTQz.XV0szA.ai5rDdGRWRFYsf1QKA0DBX0hFuA", //botunuzun tokenini yazınız
     "dashboard" : {
-      "oauthSecret": "lQRQ4QWq0VXuzlFyaTSidt5qr1xc2H8X", //botunuzun secretini yazınız
-      "callbackURL": `http://panel.leinxs.xyz/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "oauthSecret": "a4zpplghBRL3YGTGLUZLPWuRi6LZKMmb", //botunuzun secretini yazınız
+      "callbackURL": `https://konyatra.herokuapp.com/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
       "sessionSecret": "xyzxyz", //kalsın
-      "domain": "http://panel.leinxs.xyz" //site URLnizi yazınız!
+      "domain": "https://konyatra.herokuapp.com/" //site URLnizi yazınız!
     }
   };
   
